fe33aa2aa05397519635a18cdb14108b54ef4948
DevNewbee <461572989@qq.com>
2017-11-03 21:28:30 +0800

    Initial Commit

---
+++ JSPopoverMenuUITests/JSPopoverMenuUITests.swift
@@ -0,0 +1,36 @@
+//
+//  JSPopoverMenuUITests.swift
+//  JSPopoverMenuUITests
+//
+//  Created by 王俊硕 on 2017/11/3.
+//  Copyright © 2017年 王俊硕. All rights reserved.
+//
+
+import XCTest
+
+class JSPopoverMenuUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
